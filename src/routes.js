48393060a82ecf49ae0ae3600539f88b78ba9904
Pedro Alarcon <pedroa54@icloud.com>
2024-05-16 16:16:51 -0500

    adds routes

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import MainMenu from "./pages/MainMenu";
+import Splash from "./pages/Splash";
+import Cart from "./pages/cart";
+
+
+const routes = [
+    {
+    path: "/",
+    element: <Splash />
+    },
+    {
+    path: "/shop",
+    element: <MainMenu />
+    },
+    {
+    path: "/product/:id",
+    element: <Cart />
+    }
+];
+
+
+export default routes;
