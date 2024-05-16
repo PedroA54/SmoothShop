f854bb88ef9d716a379280851dd0e75dc11393b0
Pedro Alarcon <pedroa54@icloud.com>
2024-05-16 16:12:15 -0500

    adds index.js info and routing

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes";
+
+const router = createBrowserRouter(routes)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RouterProvider router={router} />);
